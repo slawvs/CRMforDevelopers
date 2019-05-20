9f77ce02a52313f6a6a58a0b71d4950d410b0597
slawvs <slawvs@gmail.com>
2019-05-20 10:08:01 +0200

    cosmetic changes to SecurityController

--- src/main/java/pl/slawek/controller/SecurityController.java
+++ src/main/java/pl/slawek/controller/SecurityController.java
@@ -1,10 +1,7 @@
 package pl.slawek.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import pl.slawek.service.UserService;
 
 @Controller
 public class SecurityController {
